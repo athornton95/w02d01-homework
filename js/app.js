b5f540a5a2d9040d6ecbb89b61d4c7d6ff90f528
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-25 15:09:19 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -8,3 +8,16 @@ console.log("Week 1 Day 1 HW");
 //list of names from class and location - object
 //list of names from class, location, and a list of tv shows - object & array within
 
+//Take it Easy
+
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet" ];
+console.log(rainbow[4]);
+
+const alex = {
+	favoriteFood: "sushi",
+	hobby: "skiing",
+	location: "Denver",
+	favoriteType: "Boolean"
+}
+
+console.log(alex.hobby);
