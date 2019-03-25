9c557d623ed2accb6f5989ea2fc266e393f091f0
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-25 15:06:20 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,10 @@
+console.log("Week 1 Day 1 HW");
+
+//Data Types
+//light swtich that can be on or off - boolean
+//users email - string
+//spaceship - array
+//list of names from class - array
+//list of names from class and location - object
+//list of names from class, location, and a list of tv shows - object & array within
+
